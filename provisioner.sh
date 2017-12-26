030335bc5d10ea5e0b069e8e9e47c8b17a3fca07
Yamin Noor <ymnoor21@gmail.com>
2017-12-25 16:34:38 -0800

    Make ansible directory and set permission

--- provisioner.sh
+++ provisioner.sh
@@ -5,4 +5,7 @@ sudo apt-get -y update &&
 	apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev &&
 	apt-get -y install python-dev &&
 	apt-get -y install python-pip &&
-	pip install --upgrade pip
+	pip install --upgrade pip &&
+	mkdir /tmp/ansible &&
+	chown ubuntu:ubuntu /tmp/ansible/ &&
+	chmod 775 /tmp/ansible
